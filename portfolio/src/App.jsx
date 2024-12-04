453b0d19f8ba7e6d226cd5c4bf44e67958cf11ef
jo4ovms <joaovictormacields@gmail.com>
2024-12-04 06:10:22 -0300

    feat: add routes main application

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,26 @@
 import "../global.css";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homer from "./pages/Home/Homer";
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <div className="main-content">
         {/* <About /> */}
-        <Homer />
-      </div>
+      {/* <Homer /> */}
+      {/* </div> */}
+
+      <Router>
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Homer />} />
+            <Route path="/sobre" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
