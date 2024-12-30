da861a4047f7d8b9ca9a4cbecb1b372781f0715a
jo4ovms <joaovictormacields@gmail.com>
2024-12-30 19:04:29 -0300

    feat: add remaining website routes and footer on website

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,25 +1,29 @@
 import "../global.css";
 import Navbar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Homer from "./pages/Home/Homer";
+import Contact from "./pages/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
-      {/* <Navbar />
-      <div className="main-content">
-        {/* <About /> */}
-      {/* <Homer /> */}
-      {/* </div> */}
-
       <Router>
         <Navbar />
         <div className="main-content">
           <Routes>
-            <Route path="/" element={<Homer />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Homer />} />
             <Route path="/sobre" element={<About />} />
+            <Route path="/contato" element={<Contact />} />
           </Routes>
         </div>
+        <Footer />
       </Router>
     </>
   );
