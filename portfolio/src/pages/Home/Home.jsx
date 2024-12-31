b1793c9add13057f86ec78e22044b8b117254c6d
jo4ovms <joaovictormacields@gmail.com>
2024-12-31 19:00:37 -0300

    feat: add improvements for home page and responsiveness

--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,262 +1,211 @@
-import React from "react";
-import { Button } from "../../components/Button";
-import { Component } from "../../components/Component";
-import { Navbar } from "../../components/Navbar";
-import { Box, Typography } from "@mui/material";
+import {
+  Typography,
+  Button,
+  Container,
+  useTheme,
+  Box,
+  useMediaQuery,
+} from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { useMemo } from "react";
+import MockupGallery from "../../components/MockupGallery/MockupGallery";
+import CalltoAction from "../../components/CalltoAction/CalltoAction";
+import DownloadIcon from "@mui/icons-material/Download";
+import EmblaCarousel from "../../components/Carousel/EmblaCarousel";
 
 const Home = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.down("lg"));
+
+  const images = useMemo(
+    () => [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/spring/spring-original-wordmark.svg",
+    ],
+    []
+  );
+
+  const OPTIONS = {
+    loop: true,
+    playOnInit: false,
+    stopOnInteraction: false,
+    dragFree: false,
+    watchDrag: false,
+    duration: 60,
+    watchFocus: false,
+  };
+
+  const SLIDES = images;
+
   return (
-    <div
-      style={{
-        width: 1920,
-        height: 1600,
-        position: "relative",
-        background: "linear-gradient(270deg, #130F40 0%, black 100%)",
+    <Container
+      sx={{
+        height: "100%",
+        padding: isSmallScreen ? 2 : isMediumScreen ? 3 : 4,
       }}
     >
-      <div
-        style={{
-          width: 472,
-          height: 18,
-          left: 1150,
-          top: 34,
-          position: "absolute",
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        sx={{
+          height: isSmallScreen || isMediumScreen ? "auto" : "450px",
+          display: "flex",
           justifyContent: "center",
-          alignItems: "flex-start",
-          gap: 55,
-          display: "inline-flex",
+          alignItems: "center",
         }}
       >
-        <div
-          style={{
-            textAlign: "center",
-            color: "white",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
+        <Grid size={{ xs: 12, sm: 9, md: 10, lg: 5 }}>
+          <Box
+            sx={{
+              position: "relative",
+              width: "100%",
+              paddingTop: "calc(100% * 16 / 16)",
+              overflow: "hidden",
+              borderRadius: "40px",
+              cursor: "pointer",
+              backgroundImage: `url(me3.jpg)`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              border: "4px #130F40 solid",
+              backgroundPosition: "center",
+            }}
+          />
+        </Grid>
+
+        <Grid
+          size={{ xs: 12, md: 12, lg: 7 }}
+          container
+          direction="column"
+          spacing={2}
         >
-          SOBRE
-        </div>
-        <div
-          style={{
-            textAlign: "center",
-            color: "white",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
-        >
-          HABILIDADES
-        </div>
-        <div
-          style={{
-            textAlign: "center",
-            color: "white",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
-        >
-          PROJETOS
-        </div>
-        <div
-          style={{
-            textAlign: "center",
-            color: "white",
-            fontSize: 15,
-            fontFamily: "Inter",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
-        >
-          CONTATO
-        </div>
-      </div>
-      <div
-        style={{
-          width: 416,
-          height: 450,
-          left: 300,
-          top: 207,
-          position: "absolute",
-          background: "#D9D9D9",
-          borderRadius: 40,
-          border: "4px #130F40 solid",
-        }}
-      />
-      <div
-        style={{
-          width: 195,
-          height: 57,
-          left: 802,
-          top: 523,
-          position: "absolute",
+          <Grid>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: isSmallScreen
+                  ? "40px"
+                  : isMediumScreen
+                  ? "50px"
+                  : isLargeScreen
+                  ? "50px"
+                  : "60px",
+                textAlign: isLargeScreen ? "center" : "left",
+                margin: isLargeScreen ? "unset" : "-10px 50px 0 90px",
+                width: isLargeScreen ? "100%" : "calc(100% - -40px)",
+              }}
+            >
+              <span
+                style={{
+                  background: "linear-gradient(to right, #fff 0%, #696969 85%)",
+                  WebkitBackgroundClip: "text",
+                  WebkitTextFillColor: "transparent",
+                }}
+              >
+                Sou um desenvolvedor back-end, entusiasta de front-end e
+                apaixonado por tecnologia.
+              </span>
+            </Typography>
+          </Grid>
+
+          <Grid>
+            <Typography
+              variant="h2"
+              sx={{
+                textAlign: isLargeScreen ? "center" : "left",
+                margin: isLargeScreen ? "unset" : "0 90px",
+                width: isLargeScreen ? "100%" : "calc(100% - -30px)",
+              }}
+            >
+              Olá, me chamo João e estou cursando Análise e Desenvolvimento de
+              Sistemas. Construo soluções escaláveis no back-end com Java e
+              interfaces modernas e responsivas no front-end com React.
+            </Typography>
+          </Grid>
+
+          <Grid
+            sx={{
+              display: "flex",
+              justifyContent: isLargeScreen ? "center" : "unset",
+            }}
+          >
+            <Button
+              variant="contained"
+              endIcon={<DownloadIcon />}
+              sx={{
+                fontSize: "16px",
+                p: "15px 30px",
+                margin: isMediumScreen ? "unset" : "0 90px",
+                mt: isMediumScreen ? "unset" : "15px",
+              }}
+            >
+              Download CV
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        sx={{ mt: isMediumScreen ? 10 : isLargeScreen ? 130 : 10 }}
+        justifyContent="center"
+      >
+        <Grid size={{ xs: 12, md: 12 }}>
+          <Typography
+            variant="h1"
+            sx={{
+              textAlign: "center",
+              fontSize: isSmallScreen
+                ? "1.5rem"
+                : isMediumScreen
+                ? "2.5rem"
+                : "3rem",
+            }}
+          >
+            Trabalhos Recentes
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 12, md: 12, lg: 12 }}>
+          <MockupGallery />
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        size={{ xs: 12, md: 12, lg: 12 }}
+        sx={{
+          mt: "100px",
+          mb: "-115px",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        <div
-          style={{
-            width: 195,
-            height: 57,
-            left: 0,
-            top: 0,
-            position: "absolute",
-            background: "#00D9FF",
-            borderRadius: 50,
-          }}
-        />
-        <div
-          style={{
-            width: 127,
-            height: 20.36,
-            left: 31,
-            top: 18.32,
-            position: "absolute",
-            color: "black",
-            fontSize: 20,
-            fontFamily: "DM Sans",
-            fontWeight: "500",
-            lineHeight: 20,
-            wordWrap: "break-word",
-          }}
-        >
-          Download CV
-        </div>
-      </div>
-      <div
-        style={{
-          width: 818,
-          height: 124,
-          left: 802,
-          top: 659,
-          position: "absolute",
-          color: "#B4B4B4",
-          fontSize: 20,
-          fontFamily: "DM Sans",
-          fontWeight: "700",
-          wordWrap: "break-word",
+        <Box>
+          <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        </Box>
+      </Grid>
+
+      <Grid
+        container
+        size={{ xs: 12, md: 12, lg: 12 }}
+        sx={{
+          mb: "-35px",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        Olá, me chamo João e sou estudante de Análise e Desenvolvimento de
-        Sistemas. Construo soluções escaláveis no back-end com Java e interfaces
-        modernas e responsivas no front-end com React.
-      </div>
-      <div
-        style={{
-          width: 818,
-          height: 183,
-          left: 802,
-          top: 216,
-          position: "absolute",
-          color: "white",
-          fontSize: 48,
-          fontFamily: "DM Sans",
-          fontWeight: "700",
-          wordWrap: "break-word",
-        }}
-      >
-        Sou um desenvolvedor back-end, entusiasta de front-end e apaixonado por
-        tecnologia.
-      </div>
-      <div
-        style={{
-          width: 800,
-          height: 600,
-          left: 216,
-          top: 958,
-          position: "absolute",
-        }}
-      >
-        <img
-          style={{
-            width: 800,
-            height: 600,
-            left: 0,
-            top: 0,
-            position: "absolute",
-          }}
-          src="https://via.placeholder.com/800x600"
-        />
-        <div
-          style={{
-            width: 105,
-            height: 45,
-            left: 684,
-            top: 27,
-            position: "absolute",
-            background: "rgba(0, 0, 0, 0.20)",
-            boxShadow: "4px 4px 4px ",
-            borderRadius: 32,
-            filter: "blur(4px)",
-          }}
-        />
-        <div
-          style={{
-            left: 704,
-            top: 39,
-            position: "absolute",
-            color: "white",
-            fontSize: 16,
-            fontFamily: "DM Sans",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
-        >
-          Stockify
-        </div>
-      </div>
-      <div
-        style={{
-          width: 800,
-          height: 600,
-          left: 1053,
-          top: 958,
-          position: "absolute",
-          background: "white",
-        }}
-      >
-        <img
-          style={{
-            width: 800,
-            height: 600,
-            left: 0,
-            top: 0,
-            position: "absolute",
-          }}
-          src="https://via.placeholder.com/800x600"
-        />
-        <div
-          style={{
-            width: 105,
-            height: 45,
-            left: 684,
-            top: 27,
-            position: "absolute",
-            background: "rgba(0, 0, 0, 0.20)",
-            boxShadow: "4px 4px 4px ",
-            borderRadius: 32,
-            filter: "blur(4px)",
-          }}
-        />
-        <div
-          style={{
-            left: 704,
-            top: 39,
-            position: "absolute",
-            color: "white",
-            fontSize: 16,
-            fontFamily: "DM Sans",
-            fontWeight: "700",
-            wordWrap: "break-word",
-          }}
-        >
-          Stockify
-        </div>
-      </div>
-    </div>
+        <CalltoAction />
+      </Grid>
+    </Container>
   );
 };
+
 export default Home;
